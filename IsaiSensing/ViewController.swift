6cabe91d4c956a6f8553ec105df762405c8bce9d
karamawariken <24karamawariken@gmail.com>
2020-01-17 15:57:37 +0900

    add comment

--- IsaiSensing/ViewController.swift
+++ IsaiSensing/ViewController.swift
@@ -42,14 +42,42 @@ class ViewController: UIViewController, MWMDelegate {
         print("disConnectBtnClick")
         mwm?.disconnectDevice()
     }
-    
+    /// 脳波データ取得時のメソッド(delta, theta Alpha(low, high))
+    /// - Parameters:
+    ///   - delta: delta value
+    ///   - theta: theta value
+    ///   - lowAlpha: lowAlpha value
+    ///   - highAlpha: highAlpah value
     func eegPowerDelta(_ delta: Int32, theta: Int32, lowAlpha: Int32, highAlpha: Int32) {
         print("delta = " ,delta)
         print("theta = " ,theta)
         print("lowAlpha = " ,lowAlpha)
         print("highAlpha = " ,highAlpha)
     }
-
+    
+    /// 脳波データ取得時のメソッド(Beta(low, high), Gamma(low, high)
+    /// - Parameters:
+    ///   - lowBeta: lowBeta value
+    ///   - highBeta: highBeta value
+    ///   - lowGamma: lowGamma value
+    ///   - midGamma:  midGamma value
+    func eegPowerLowBeta(_ lowBeta: Int32, highBeta: Int32, lowGamma: Int32, midGamma: Int32) {
+        print("lowBeta = " ,lowBeta)
+        print("highBeta = " ,highBeta)
+        print("lowGamma = " ,lowGamma)
+        print("lowGamma = " ,midGamma)
+    }
+    
+    /// 脳波データ取得時のメソッド(poorSignal, attention meditation)
+    /// - Parameters:
+    ///   - poorSignal: 繋がっていれば0 ずれたりすると数値が上がる
+    ///   - attention: 集中度(NeuroSky独自ロジック)
+    ///   - meditation: 瞑想度(NeuroSky独自ロジック)
+    func eSense(_ poorSignal: Int32, attention: Int32, meditation: Int32) {
+        print("poorSignal = " ,poorSignal)
+        print("attention = " ,attention)
+        print("meditation = " ,meditation)
+    }
 
 }
 
