f005e2896aab73ef998c1c18bc3b8d008c8b813a
karamawariken <24karamawariken@gmail.com>
2020-01-17 12:55:57 +0900

    add MindWaveMobile2 SDK files

---
+++ IsaiSensing/IsaiSensing_Bridging_Header.h
@@ -0,0 +1,17 @@
+//
+//  IsaiSensing-Bridging-Header.h
+//  IsaiSensing
+//
+//  Created by nishi kosei on 2020/01/16.
+//  Copyright © 2020 nishi kosei. All rights reserved.
+//
+
+#ifndef IsaiSensing_Bridging_Header_h
+#define IsaiSensing_Bridging_Header_h
+
+
+#endif /* IsaiSensing_Bridging_Header_h */
+
+#import "MWMDevice.h"
+#import "MWMDelegate.h"
+#import "MWMEnum.h"
--- IsaiSensing/ViewController.swift
+++ IsaiSensing/ViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MWMDelegate {
+    
+    let mwm = MWMDevice.sharedInstance()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mwm?.delegate = self
+    }
+    
+    func deviceFound(_ devName: String!, mfgID: String!, deviceID: String!) {
+        print("devName = "+devName)
+        print("mfgID = "+mfgID)
+        print("deviceID = "+deviceID)
+        //choose the correct device and connect
+        mwm?.connect(deviceID)
+    }
+    
+    func didConnect() {
+        print("didconnect")
+    }
+    
+    func didDisconnect() {
+        print("didDisconnect");
+    }
+    @IBAction func ScanButton(_ sender: Any) {
+        print("scanBtnClick")
+        mwm?.scanDevice()
+    }
+    
+    @IBAction func disConnectButton(_ sender: Any) {
+        print("disConnectBtnClick")
+        mwm?.disconnectDevice()
+    }
+    
+    func eegPowerDelta(_ delta: Int32, theta: Int32, lowAlpha: Int32, highAlpha: Int32) {
+        print("delta = " ,delta)
+        print("theta = " ,theta)
+        print("lowAlpha = " ,lowAlpha)
+        print("highAlpha = " ,highAlpha)
     }
 
 
